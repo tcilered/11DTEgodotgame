6cae32e3ad3c95ff7589da3e090ab92f3ad9d970
tcilered <thomasx994@gmail.com>
2024-07-26 03:36:32 +1200

    Update Projectile1.gd

--- Projectile1.gd
+++ Projectile1.gd
@@ -5,8 +5,7 @@ var travelled_distance = 0
 
 func _physics_process(delta):
 	const SPEED = 1000
-	const RANGE = 100
-	
+	const RANGE = 1200
 	
 	var direction = Vector2.RIGHT. rotated(rotation)
 	position += direction * SPEED * delta
