d9f722ad9e740d960b04e3f27c52c7ce0f91ce24
tcilered <thomasx994@gmail.com>
2024-09-19 23:21:09 +1200

    Pathfinding

--- scenes/wolf.gd
+++ scenes/wolf.gd
@@ -1,19 +1,28 @@
 extends CharacterBody2D
 
+var speed = 300
+var accel = 7
+
 var active: bool = false
+@onready var nav: NavigationAgent2D = $NavigationAgent2D
 @onready var player = get_node("/root/Main/World/Player")
 
-func _ready():
-	$NavigationAgent2D.target_position = player.global_rotation
-	print($NavigationAgent2D.get_current_navigation_path())
+func _physics_process(delta: float) -> void:
+	var direction = Vector3()
+	$NavigationAgent2D.target_position = get_global_mouse_position()
+	direction = nav.get_next_path_position() - global_position
+	direction = direction.normalized()
+	velocity = velocity.lerp(direction * speed, accel * delta)
 
 func _process(delta):
-	
+	pass
 	move_and_slide()
 
 func _on_notice_area_body_entered(body: Node2D) -> void:
+	pass
 	active = true
 
 
 func _on_notice_area_area_shape_exited(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	pass
 	active = false
