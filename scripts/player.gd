270180b487ea4bd8bce69a4b5fb5f7f4046bef89
tcilered <thomasx994@gmail.com>
2024-09-27 21:37:12 +1200

    Revert "added to bullets & enemies"
    This reverts commit 648e7ca1a7b07f1e934f4f1a5554e4f10e7d6e02.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,6 @@
 extends CharacterBody2D
-var health = 100.0
-
-signal health_depleted
 
 func _physics_process(delta):
 	var direction = Input.get_vector("left","right","up","down")
 	velocity = direction * 200
 	move_and_slide()
-
-	const DAMAGE_RATE = 5.0
-
-	var overlapping_mobs = %Hurtbox.get_overlapping_bodies()
-	if overlapping_mobs.size() > 0:
-		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
-		if health <= 0.0:
-			health_depleted.emit()
